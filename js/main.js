26a167f759c019001786eccb9ada0dac67b20341
Joe A. Simpson, Jr <joesimpsonjr@users.noreply.github.com>
2015-03-07 08:08:27 -0800

    Update site based on recent changes
    Battery is running out; need to transition to another computer.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-$(document).ready(function(){
-
-	// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-	var mainbottom = $('#hero-01').offset().top + $('#hero-01').height();
-
-	// on scroll, 
-	$(window).on('scroll',function(){
-
-    // we round here to reduce a little workload
-    stop = Math.round($(window).scrollTop());
-    if (stop > mainbottom) {
-        $('.nav').addClass('past-main');
-    } else {
-        $('.nav').removeClass('past-main');
-    }
-
-});
+function toggleDiv(divId) {
+   $("#"+divId).toggle();
+}
